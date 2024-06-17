11f517a41d311b49b66c2c7c3a4ef447bb12a771
tarun <kandula.tarun99895@gmail.com>
2024-06-17 00:51:21 -0400

    Initial Commit with Search bar logic

---
+++ src/components/MoviesGrid.js
@@ -0,0 +1,39 @@
+import React, { useState, useEffect } from "react";
+import "../styles.css";
+import MovieCard from "./MovieCard";
+
+export default function MoviesGrid() {
+  const [movies, setMovies] = useState([]);
+  const [searchTerm, setsearchTerm] = useState("");
+
+  useEffect(() => {
+    fetch("movies.json")
+      .then((response) => response.json())
+      .then((data) => setMovies(data));
+  }, []);
+
+  const handleSearchChange = (e) => {
+    setsearchTerm(e.target.value);
+  };
+
+  const filteredMovies = movies.filter((movie) =>
+    movie.title.toLowerCase().includes(searchTerm.toLowerCase())
+  );
+
+  return (
+    <div>
+      <input
+        type="text"
+        className="search-input"
+        placeholder="Search Movies ..."
+        value={searchTerm}
+        onChange={handleSearchChange}
+      ></input>
+      <div className="movies-grid">
+        {filteredMovies.map((movie) => (
+          <MovieCard movie={movie} key={movie.id}></MovieCard>
+        ))}
+      </div>
+    </div>
+  );
+}
